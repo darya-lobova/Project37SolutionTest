d6edb32102314f3d45f6fea358e6c03be47b13b1
darya-loboca <dasha.lobova.it@gmail.com>
2026-02-05 19:16:38 +0300

    task 01 developing

---
+++ Task1/main.cpp
@@ -0,0 +1,15 @@
+#include "logic.h"
+
+int main() {
+
+	int x1, y1;
+	int x2, y2;
+
+	cout << "Input coordinates of the first square: ";
+	cin >> x1 >> y1;
+
+	cout << "Input coordinates of the second square: ";
+	cin >> x2 >> y2;
+
+	return 0;
+}
